8d9ad918afbf3cdc8d9ab0bbc7933e1c64e6f7c1
ayalamih <31007354@qq.com>
2018-09-24 21:27:09 +0800

    test(core)
    test gitignore
    
    Close issues #1

--- src/main/java/com/ayalamih/hellobase/App.java
+++ src/main/java/com/ayalamih/hellobase/App.java
@@ -1,13 +1,42 @@
 package com.ayalamih.hellobase;
 
 /**
- * Hello world!
- *
+ * @author ayalamih
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        int j = 9;
+        int m = j--;
+        System.out.println(m);
+
+        int i = 9;
+        int n = --i;
+        System.out.println(n);
+
+        int[] nums = new int[]{1, 5, 9, 43, 90, 4, 5, 6};
+        int target = 10;
+        int[] result = getTwo(nums, target);
+        for (int x = 0; x < result.length; x++) {
+            System.out.println(result[x]);
+        }
+    }
+
+    private static int[] getTwo(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+
+            for (int j = nums.length - 1; j > 0; j--) {
+                int b = nums[j];
+
+                if (a + b == target) {
+                    result = new int[]{i, j};
+                }
+            }
+        }
+
+        return result;
     }
 }
---
+++ src/main/java/com/ayalamih/hellobase/design/Test.java
@@ -0,0 +1,15 @@
+package com.ayalamih.hellobase.design;
+
+/**
+ * Created on 2018/09/23.
+ *
+ * @author echo
+ * @version 0.0.0.0 Sep 23, 2018.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+
+}
